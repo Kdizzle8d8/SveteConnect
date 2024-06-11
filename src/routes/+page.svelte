23548fb1d09c00a0a44d4fcea13ad318c2c3158d
Kyle <kyle@kyle.stream>
2024-06-11 15:44:31 -0500

    Added download link for apk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
 	import { Button } from '$lib/components/ui/button';
+	import { Download } from 'lucide-svelte';
 </script>
 
-<h1 class="place-self-center text-2xl font-bold">This is the landing page!</h1>
-<Button variant="link" href="/puzzle">Puzzles</Button>
+<div class="p-2">
+	<h1 class="place-self-center text-2xl font-bold">Welcome to SvelteConnect!</h1>
+	<p>This is a clone of the popular game <a class="text-blue-500 hover:underline" href="https://www.nytimes.com/games/connections">Connections</a> by the New York Times Made with Svelte 5</p>
+	<p>There is also mobile builds of this game created using <a class="text-blue-500 hover:underline" href="https://capacitorjs.com/">Capacitor by Ionic </a>that can run completley offline!</p>
+	<Button href="https://drive.proton.me/urls/DCEYMJH8B4#gi84I1ZGZBS1" class="mt-10">Download APK <Download class="ml-2 size-5" /></Button>
+</div>
